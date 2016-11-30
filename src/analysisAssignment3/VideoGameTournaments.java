7b69475147d98467fd2517cc48f41b190f076439
Tokyo <ahmed.tokyo1@gmail.com>
2016-12-01 00:04:53 +0200

    initialezed assignment3Question2

--- src/analysisAssignment3/VideoGameTournaments.java
+++ src/analysisAssignment3/VideoGameTournaments.java
@@ -10,6 +10,8 @@
  */
 package analysisAssignment3;
 
+import java.util.ArrayList;
+
 /**
  * @author Tokyo
  *
@@ -19,11 +21,34 @@ public class VideoGameTournaments {
 	
 	public static void fillDPArray(int [] difficulties, int []prizes) {
 		dpArray = new int [difficulties.length][difficulties.length];
+		// TODO solve
+	}
+	
+	public static ArrayList<Integer> getTournaments(int [] difficulties, int []prizes) {
+		fillDPArray(difficulties, prizes);
+		ArrayList<Integer> tournaments = new ArrayList<Integer>();
+		// TODO trace dpArray
+		return tournaments;
+	}
+	
+	public static String arrayToString(int [] [] array) {
+		String result = "";
+		for(int i = 0; i<array.length; i++){
+			result+=("[ ");
+		    for(int j = 0; j<array[i].length; j++){
+		    	result+=(array[i][j] + (j==array.length-1?" ]":", "));
+		    }
+		    result+="\n";
+		}
+		return result;
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int [] difficulties = {};
+		int [] prizes = {};
+		ArrayList<Integer> tournaments = getTournaments(difficulties, prizes);
+		System.out.println(tournaments);
+		System.out.println(arrayToString(dpArray));
 	}
 
 }
